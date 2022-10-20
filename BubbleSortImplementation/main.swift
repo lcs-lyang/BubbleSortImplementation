c32220fabdee849e90e64ffc7932266ff1f4662a
Lillian Yang <lyang@lcs.on.ca>
2022-10-20 11:42:49 -0400

    Made the list and added 3 values to it.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -9,3 +9,14 @@ import Foundation
 
 print("Hello, World!")
 
+// create an empty list (array)
+//telling swift that we want a lift of integers that starts empty
+var dataSet: [Int] = []
+
+//Populate the list
+dataSet.append(18)
+dataSet.append(36)
+dataSet.append(9)
+
+//Print the list
+print(dump(dataSet))
