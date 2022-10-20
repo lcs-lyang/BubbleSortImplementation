eb2168e280ace4c997664cc0edd938947f8b86bc
Lillian Yang <lyang@lcs.on.ca>
2022-10-20 12:20:28 -0400

    Finished bubble sort so that an array with "n" numbers is passed over "n" times.

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -32,21 +32,29 @@ print("Unsorted:")
 print(dataSet)
 waitForUserInput()
 
-//One pass through the array to float the highest number to the end
-for j in 0...dataSet.count - 1 {
-    print(j)
+//Loop through the entire array "n" times
+//(however many time there are elements in the array)
+for i in 0..<dataSet.count {
     
-    //Compare left valur to right value
-    if dataSet[j] > dataSet[j + 1] {
-        //swap values (when left value is more than right value)
-        let temporaryValue = dataSet[j] //set asaide the left value
-        dataSet[j] = dataSet[j + 1] //replace left with right
-        dataSet[j + 1] = temporaryValue //replace right with the temporary value
+    //One pass through the array to float the highest number to the end
+    for j in 0...dataSet.count - 1 {
+        print(j)
+        
+        //Compare left valur to right value
+        if dataSet[j] > dataSet[j + 1] {
+            //swap values (when left value is more than right value)
+            let temporaryValue = dataSet[j] //set asaide the left value
+            dataSet[j] = dataSet[j + 1] //replace left with right
+            dataSet[j + 1] = temporaryValue //replace right with the temporary value
+        }
     }
+
+    //Print the array after the pass
+
+    print("Array after pass:")
+    print(dataSet)
+    waitForUserInput()
+    
 }
 
-//Print the array after the pass
 
-print("Array after pass:")
-print(dataSet)
-waitForUserInput()
