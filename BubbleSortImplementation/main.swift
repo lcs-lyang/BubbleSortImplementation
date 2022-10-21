99045f3b0483a18f704e03da6f1bdd608b2904cd
Lillian Yang <lyang@lcs.on.ca>
2022-10-21 14:01:12 -0400

    Fixed the error. If you put (...dataCount), it will do comparisons from 0-10 which is 11 comparisons. However, we have set to compare only 10 numbers which is (0-9). Hence that's why there is an error stating "out of index" because it was doing 11 comparisons instead of 10. This is why (..<dataCount) is used...

--- BubbleSortImplementation/main.swift
+++ BubbleSortImplementation/main.swift
@@ -14,8 +14,6 @@ func waitForUserInput() {
     _ = readLine()
 }
 
-print("Hello, World!")
-
 // create an empty list (array)
 //telling swift that we want a lift of integers that starts empty
 var dataSet: [Int] = []
@@ -38,21 +36,28 @@ for i in 0..<dataSet.count {
     
     //One pass through the array to float the highest number to the end
     
-
-    for j in 0...dataSet.count - 1 {
+    
+    for j in 0..<dataSet.count - 1 {
         print(j)
         
         //Compare left valur to right value
         print("Comparison \(j + 1)...", terminator: "")
+        
         if dataSet[j] > dataSet[j + 1] {
+            
             //swap values (when left value is more than right value)
-            let temporaryValue = dataSet[j] //set asaide the left value
+            
+            let temporaryValue = dataSet[j] //set aside the left value
+            
             dataSet[j] = dataSet[j + 1] //replace left with right
+            
             dataSet[j + 1] = temporaryValue //replace right with the temporary value
+            
             print("values were swapped.", terminator: "")
         }
     }
     
+
     print("")
 
     //Print the array after the pass
